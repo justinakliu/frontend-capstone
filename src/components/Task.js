843c69c17ef48d09356ac93324c1fcd75682156b
Justina <justinakliu@gmail.com>
2023-01-25 18:59:46 -0500

    added conditional styling for task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Flex, Box, Heading, Spacer, Button } from "@chakra-ui/react";
+import { Flex, Box, Spacer, Button, Heading } from "@chakra-ui/react";
 
 const Task = ({ subgoal, handleUpdateGoalComplete }) => {
   return (
-    <Flex minWidth="max-content" alignItems="center" gap="2">
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      bg={subgoal.complete ? "grey" : "white"}
+    >
       <Box p="2">
-        <Heading size="md">
-          {" "}
-          <h2>{subgoal.name}</h2>
-        </Heading>
+        <Heading size="md">{subgoal.name}</Heading>
       </Box>
       <Spacer />
       <Button onClick={() => {}}>
